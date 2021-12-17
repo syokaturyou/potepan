9a17fe4f4fb57af8f65d2fc724fb245f91e85195
syokaturyou <teamsumagaku2@gmail.com>
2021-12-17 13:28:47 +0000

    javascript勉強

---
+++ test/lesson.js
@@ -0,0 +1,17 @@
+let userName = "ポテパン太郎";// 変数を宣言する
+let userAge = 28; // 変数を宣言する
+ 
+console.log(userName);
+console.log(userAge);
+ 
+let message = "誕生日がきた！！"; // 新たに変数を宣言する
+userAge = 29; // 変数の中の値を変更する
+ 
+console.log(message);
+console.log(userAge);
+ 
+message = "おめでとう！！"; // 新たに変数の中身を変更する
+ 
+console.log(message);
+console.log(message);
+console.log(message);
