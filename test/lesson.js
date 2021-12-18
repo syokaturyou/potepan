39d845d31a09a716f950b3c5c573e1801cb2665e
syokaturyou <teamsumagaku2@gmail.com>
2021-12-18 09:23:26 +0000

    javascript

--- test/lesson.js
+++ test/lesson.js
@@ -1,17 +1,29 @@
-let userName = "ポテパン太郎";// 変数を宣言する
-let userAge = 28; // 変数を宣言する
+// let userName = "ポテパン太郎";// 変数を宣言する
+// let userAge = 28; // 変数を宣言する
  
-console.log(userName);
-console.log(userAge);
+// console.log(userName);
+// console.log(userAge);
  
-let message = "誕生日がきた！！"; // 新たに変数を宣言する
-userAge = 29; // 変数の中の値を変更する
+// let message = "誕生日がきた！！"; // 新たに変数を宣言する
+// userAge = 29; // 変数の中の値を変更する
  
-console.log(message);
-console.log(userAge);
+// console.log(message);
+// console.log(userAge);
  
-message = "おめでとう！！"; // 新たに変数の中身を変更する
+// message = "おめでとう！！"; // 新たに変数の中身を変更する
  
-console.log(message);
-console.log(message);
-console.log(message);
+// console.log(message);
+// console.log(message);
+// console.log(message);
+
+let name = "ポテパン太郎";
+console.log("こんにちは！" + name + "さん");
+console.log(name + "さんの注文履歴はこちらです");
+console.log(name + "さんにおすすめの商品はこちらです");
+    
+let message = "こんにちは！";
+console.log(message + "ポテパン太郎さん");
+console.log(message + "佐藤javascriptさん");
+console.log(message + "html鈴木さん");
+    
+    
