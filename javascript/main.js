578ed21d1e35860d91c4597a5a81f4e8cf427de9
syokaturyou <teamsumagaku2@gmail.com>
2021-12-24 09:21:32 +0000

    12/24 4

--- javascript/main.js
+++ javascript/main.js
@@ -1,48 +1,58 @@
- 
- 
- $(document).ready(function(){
-      $(".buttonA").click(function() {
-      let get_text = $(".target").text(); //テキストを取得し変数に入れています。
-      $(".target").text("aaa");
-      // alert(get_text); //テキストが取得できたかアラートで確認をします。
+
+     $(document).ready(function(){
+      $(".button0").click(function() {
+      let value = $(".button0").text(); //テキストを取得し変数に入れています。
+      $(".target").text(value); //テキストを変更する処理です。
       });
  
-      $(".buttonB").click(function() {
-       $(".target").text("bbbb"); //テキストを変更する処理です。
+      $(".button1").click(function() {
+      let value = 10 * value + $(".button1").text(); 
+       $(".target").text(value); 
+      });
+      
+      $(".button2").click(function() {
+      let value = 10 * value + $(".button2").text(); 
+       $(".target").text(value); 
       });
  
-      $(".buttonC").click(function() {
-       $(".target").text("cccc"); //テキストを変更する処理です。
+      $(".button3").click(function() {
+      let value = 10 * value + $(".button3").text(); 
+       $(".target").text(value); 
+      });
+ 
+      $(".button4").click(function() {
+      let value = $(".button4").text(); 
+       $(".target").text(value); 
+      });
+      
+      $(".button5").click(function() {
+      let value = $(".button5").text(); 
+       $(".target").text(value); 
+      });
+ 
+      $(".button6").click(function() {
+      let value = $(".button6").text(); 
+       $(".target").text(value); 
+      });
+      
+      $(".button7").click(function() {
+      let value = $(".button7").text(); 
+       $(".target").text(value); 
+      });
+      
+      $(".button8").click(function() {
+      let value = $(".button8").text(); 
+       $(".target").text(value); 
+      });
+ 
+      $(".button9").click(function() {
+      let value = $(".button9").text(); 
+       $(".target").text(value); 
       });
       
       $(".buttonreset").click(function() {
-       $(".target").text("|"); //テキストを変更する処理です。
+       $(".target").text(0); 
       });
+      
   });
   
-  
-//   document.querySelectorAll('.button').forEach(function (button) {
-// 	button.addEventListener('click', {value: `${button.value}`, handleEvent: onClickButton});
-// })
-
-// function onClickButton() {
-// 	console.log(`${this.value}がクリックされました`)
-// }
- 
-//   $(document).ready(function(){
-//   $(".button_a").click(function() {
-//     $(".box_a").addClass("add_color_yellow"); //クラスの追加の処理です。
-//   });
- 
-//   $(".button_b").click(function() {
-//     $(".box_a").removeClass("add_color_yellow"); //クラスの削除の処理です。
-//   });
- 
-//   $(".button_c").click(function() {
-//     $(".box_b").addClass("add_hidden"); //display: none:のクラスを追加する処理です。
-//   });
- 
-//   $(".button_d").click(function() {
-//     $(".box_b").removeClass("add_hidden"); //display: none:のクラスを削除する処理です。
-//   });
-// });
