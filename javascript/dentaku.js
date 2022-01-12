b1d44db6950910df05b3f5aa17eabc66074fcb11
syokaturyou <teamsumagaku2@gmail.com>
2022-01-12 10:55:26 +0000

    1/12 2

--- javascript/dentaku.js
+++ javascript/dentaku.js
@@ -3,35 +3,17 @@
   //   }function append( _v ) {　　　　　　　　　　　　　// 数字ボタンが押されたので数字を後ろに追加する
   //       document.querySelector( 'input' ).value += _v 
   //   }function calc() {　　　　　　　　　　　　　　　　　// 「＝」ボタン押下時に計算
-  //       let v = document.querySelector( 'input' ).value 
-  //       let f = new Function( 'return ' + v )
+  //       const v = document.querySelector( 'input' ).value 
+  //       const f = new Function( 'return ' + v )
   //       update(f().toString())
   //   }
     
-    function get_calc(btn) {
-      if(btn.value == "=") {
-        document.dentaku.display.value = eval(document.dentaku.display.value);
-      } else {
-        document.dentaku.display.value += btn.value;
-      }
-    };  
-    
-    function get_update(btn) {
+    function append(btn) {                      // ボタン押下時に後ろに追加する。数字、四則演算、小数点等
+      document.dentaku.display.value += btn.value;
+    } function calc(btn) {                      // 「＝」ボタン押下時に計算
+      document.dentaku.display.value = eval(document.dentaku.display.value);
+    } function update(btn) {                    // 「AC」ボタン押下時にinput tag を更新する関数
       document.dentaku.display.value = "";
     }
     
-    
-    // function calc(btn) {
-    //   if(btn.value == "=") {
-    //     document.dentaku.display.value = eval(document.dentaku.display.value);
-    //   } else {
-    //     document.dentaku.display.value += btn.value;
-    //   }
-    // };
-    // // function append(btn) {
-    // //   document.dentaku.display.value += btn.value;
-    // // }
-    // function update(btn) {
-    //   document.dentaku.display.value = "";
-    // }
-    
+  
