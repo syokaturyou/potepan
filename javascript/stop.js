28f18b058464d07b608a9312bbd0f4a5afb5b6f4
syokaturyou <teamsumagaku2@gmail.com>
2022-01-12 11:42:38 +0000

    stopwatch

--- javascript/stop.js
+++ javascript/stop.js
@@ -1,80 +1,106 @@
- "use strict";
-window.addEventListener( "DOMContentLoaded" , ()=> {
-    /**
-     * @param watchCallBack 経過時間報告用コールバック
-     * @param wrapCallBack ラップ報告用コールバック
-     */
-    const getStopWatch = function ( watchCallBack , wrapCallBack ){
-        let accumulatedTime = 0,    // 積算時間
-            currentTime=null,       // タイマー開示タイムスタンプ
-            timerId=null;           // setInterval() の返り値
- 
-            // リセット処理
-        const reset = () =>{
-            timerOff(); accumulatedTime = 0; currentTime=null;
-                // リセットされたことをnullで通知
-            watchCallBack( null ); wrapCallBack ( null );
-        };
-            // 開始処理
-        const start = () =>{ currentTime = Date.now();timerOn(); };
-            // 一時停止処理
-        const pause = () =>{
-                // これまでの経過時間を退避
-            accumulatedTime = getNowTime();
-            timerOff();
-            currentTime = null;
-        };
-            // 再開処理
-        const resume = () =>start();
-            // ラップ報告処理
-        const wrap = () =>wrapCallBack( getNowTime() );
-            // 経過時間の算出
-        const getNowTime = () =>accumulatedTime + Date.now() - currentTime;
- 
-            // タイマー停止処理
-        const timerOff = () => {
-            if( timerId === null ) return;
-            clearInterval(timerId);
-            timerId = null;
-        };
-            // タイマー開始処理
-        const timerOn = () => {
-            if( timerId !== null ) clearTimeout(timerId);
-            timerId = setInterval(()=>watchCallBack( getNowTime() ),10);
-        };
- 
-        reset();
- 
-            // 必要な機能だけ返す
-        return Object.freeze({
-            start:()=> currentTime === null && accumulatedTime === 0 ?   start()  : reset(),
-            pause:()=> currentTime === null ? ( accumulatedTime === 0 ? false : resume() ) : pause(),
-            wrap:()=> currentTime === null ?  false : wrap(),
-        });
-    };
- 
-        // ミリ秒を画面表示する形式に変換
-    const timeString = time =>`${
-        Math.floor(time / 60000).toString().padStart(2,"00")
-    }:${
-        Math.floor(time % 60000 / 1000).toString().padStart(2,"00")
-    }.${
-        Math.floor(time % 1000).toString().padStart(3,"000").slice(0,2)
-    }`;
- 
-    const watchArea = document.getElementById("watchArea");
-    const wrapArea = document.getElementById("wrapArea");
- 
-    const stopWatchObj = getStopWatch(
-         time => watchArea.textContent = time === null ? "00:00.00" : timeString( time ) ,
-         time => wrapArea.value = time === null ? "" : wrapArea.value + "\n" + timeString( time )
-    );
- 
-    const buttonDefine = [
-            { id:"start" , listener:()=>stopWatchObj.start() },
-            { id:"pause" , listener:()=>stopWatchObj.pause() },
-            { id:"wrap"  , listener:()=>stopWatchObj.wrap()  }
-        ];
-    buttonDefine.forEach( e => document.getElementById(e.id).addEventListener("click",e.listener));
-});
- 
+
+ 'use strict';
+ (function(){
+    // 'use strict';
+
+    //htmlのidからデータを取得
+    //取得したデータを変数に代入
+
+    var timer = document.getElementById('timer');
+    var start = document.getElementById('start');
+    var stop = document.getElementById('stop');
+    var reset = document.getElementById('reset');
+
+    //クリック時の時間を保持するための変数定義
+    var startTime;
+
+    //経過時刻を更新するための変数。 初めはだから0で初期化
+    var elapsedTime = 0;
+
+    //タイマーを止めるにはclearTimeoutを使う必要があり、そのためにはclearTimeoutの引数に渡すためのタイマーのidが必要
+    var timerId;
+
+    //タイマーをストップ -> 再開させたら0になってしまうのを避けるための変数。
+    var timeToadd = 0;
+
+
+    //ミリ秒の表示ではなく、分とか秒に直すための関数, 他のところからも呼び出すので別関数として作る
+    //計算方法として135200ミリ秒経過したとしてそれを分とか秒に直すと -> 02:15:200
+    function updateTimetText(){
+
+        //m(分) = 135200 / 60000ミリ秒で割った数の商　-> 2分
+        var m = Math.floor(elapsedTime / 60000);
+
+        //s(秒) = 135200 % 60000ミリ秒で / 1000 (ミリ秒なので1000で割ってやる) -> 15秒
+        var s = Math.floor(elapsedTime % 60000 / 1000);
+
+        //ms(ミリ秒) = 135200ミリ秒を % 1000ミリ秒で割った数の余り
+        var ms = elapsedTime % 1000;
+
+
+        //HTML 上で表示の際の桁数を固定する　例）3 => 03　、 12 -> 012
+        //javascriptでは文字列数列を連結すると文字列になる
+        //文字列の末尾2桁を表示したいのでsliceで負の値(-2)引数で渡してやる。
+        m = ('0' + m).slice(-2); 
+        s = ('0' + s).slice(-2);
+        ms = ('0' + ms).slice(-3);
+
+        //HTMLのid　timer部分に表示させる　
+        timer.textContent = m + ':' + s + ':' + ms;
+    }
+
+
+    //再帰的に使える用の関数
+    function countUp(){
+
+        //timerId変数はsetTimeoutの返り値になるので代入する
+        timerId = setTimeout(function(){
+
+            //経過時刻は現在時刻をミリ秒で示すDate.now()からstartを押した時の時刻(startTime)を引く
+            elapsedTime = Date.now() - startTime + timeToadd;
+            updateTimetText()
+
+            //countUp関数自身を呼ぶことで10ミリ秒毎に以下の計算を始める
+            countUp();
+
+        //1秒以下の時間を表示するために10ミリ秒後に始めるよう宣言
+        },10);
+    }
+
+    //startボタンにクリック時のイベントを追加(タイマースタートイベント)
+    start.addEventListener('click',function(){
+
+        //在時刻を示すDate.nowを代入
+        startTime = Date.now();
+
+        //再帰的に使えるように関数を作る
+        countUp();
+    });
+
+    //stopボタンにクリック時のイベントを追加(タイマーストップイベント)
+    stop.addEventListener('click',function(){
+
+        //タイマーを止めるにはclearTimeoutを使う必要があり、そのためにはclearTimeoutの引数に渡すためのタイマーのidが必要
+       clearTimeout(timerId);
+
+
+        //タイマーに表示される時間elapsedTimeが現在時刻かたスタートボタンを押した時刻を引いたものなので、
+        //タイマーを再開させたら0になってしまう。elapsedTime = Date.now - startTime
+        //それを回避するためには過去のスタート時間からストップ時間までの経過時間を足してあげなければならない。elapsedTime = Date.now - startTime + timeToadd (timeToadd = ストップを押した時刻(Date.now)から直近のスタート時刻(startTime)を引く)
+       timeToadd += Date.now() - startTime;
+    });
+
+    //resetボタンにクリック時のイベントを追加(タイマーリセットイベント)
+    reset.addEventListener('click',function(){
+
+        //経過時刻を更新するための変数elapsedTimeを0にしてあげつつ、updateTimetTextで0になったタイムを表示。
+        elapsedTime = 0;
+
+        //リセット時に0に初期化したいのでリセットを押した際に0を代入してあげる
+        timeToadd = 0;
+
+        //updateTimetTextで0になったタイムを表示
+        updateTimetText();
+
+    });
+})();
