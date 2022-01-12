1e2b75e44f6da653d5ab3a94f57c105be7ac9398
syokaturyou <teamsumagaku2@gmail.com>
2022-01-12 11:21:41 +0000

    電卓終了

--- javascript/dentaku.js
+++ javascript/dentaku.js
@@ -1,19 +1,21 @@
-  // 　function update( _v ) {
-  //       document.querySelector( 'input' ).value = _v // 「AC」ボタン押下時にinput tag を更新する関数
-  //   }function append( _v ) {　　　　　　　　　　　　　// 数字ボタンが押されたので数字を後ろに追加する
-  //       document.querySelector( 'input' ).value += _v 
-  //   }function calc() {　　　　　　　　　　　　　　　　　// 「＝」ボタン押下時に計算
-  //       const v = document.querySelector( 'input' ).value 
-  //       const f = new Function( 'return ' + v )
-  //       update(f().toString())
-  //   }
-    
-    function append(btn) {                      // ボタン押下時に後ろに追加する。数字、四則演算、小数点等
-      document.dentaku.display.value += btn.value;
-    } function calc(btn) {                      // 「＝」ボタン押下時に計算
-      document.dentaku.display.value = eval(document.dentaku.display.value);
-    } function update(btn) {                    // 「AC」ボタン押下時にinput tag を更新する関数
-      document.dentaku.display.value = "";
+  // 計算時にevalを使用するものとしない物の二通り用意。
+  
+  　function append( _v ) {　　　　　　　　　　　　 // 数字・演算子・小数点ボタン等が押された際に内容追加
+        document.querySelector( 'input' ).value += _v 
+    }function update( _v ) {　　　　　　　　　　　　// 「AC」ボタン押下時にinput tag を更新する関数
+        document.querySelector( 'input' ).value = _v 
+    }function calc() {　　　　　　　　　　　　　　　// 「＝」ボタン押下時に計算
+        const v = document.querySelector( 'input' ).value 
+        const f = new Function( 'return ' + v )
+        update(f().toString())
     }
+    
+    // function append(btn) {                       // 数字・演算子・小数点ボタン等が押された際に内容追加
+    //   document.dentaku.display.value += btn.value;
+    // } function calc() {                          // 「＝」ボタン押下時に計算
+    //   document.dentaku.display.value = eval(document.dentaku.display.value);
+    // } function update(btn) {                     // 「AC」ボタン押下時にinput tag を更新する関数
+    //   document.dentaku.display.value = "";
+    // }
     
   
