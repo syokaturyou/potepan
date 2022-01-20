01dccd68fcf622ba172325df91dcf92e8f450486
syokaturyou <teamsumagaku2@gmail.com>
2022-01-20 17:35:00 +0000

    1/21 1

---
+++ animal.rb
@@ -0,0 +1,18 @@
+# animals配列を定義する
+    animals = ["パンダ", "コアラ", "カンガルー"]
+ 
+    puts animals[0]
+    puts animals[1]
+    puts animals[2]
+ 
+    puts "---------------"
+    # インデックスが1のオブジェクトを更新する
+    animals[1] = "キリン"
+    
+ 
+    animals << "ライオン"
+ 
+    puts animals[0]
+    puts animals[1]
+    puts animals[2]
+    puts animals[3]
